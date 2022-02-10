cdf5a81b6fe0f7eb5bfa7f48e235d7be562db141
Al - Amin <alaminbinmannan2@gmail.com>
2022-02-10 21:01:41 +0600

    countdown_time refactored -> Countdown.py

---
+++ Countdown.py
@@ -0,0 +1,23 @@
+import sys
+import time
+
+
+class CountDown:
+
+    import time
+    def countdown_time(self, time_length):
+        """
+     :param time_length:
+     This method divide seconds
+        """
+        print(f'Your countdown length is {time_length} minutes')
+        seconds_length = int(time_length) * 60
+        while seconds_length:
+            mins, secs = divmod(seconds_length, 60)
+            timer = '{:02d}:{:02d}'.format(mins, secs)
+            sys.stdout.write("\r%s" % timer)
+            sys.stdout.flush()
+            time.sleep(1)
+            seconds_length -= 1
+
+        print(' STOP!')
--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print('Initialize Countdown clock and timer project')
+
+from Countdown import *
+
+time_length = input('Please give a time length: ')
+
+countdown = CountDown()
+
+countdown.countdown_time(time_length)
